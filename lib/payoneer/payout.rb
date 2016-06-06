562e6ae328396d519e52062dd1719f3c8befd4d4
NM <nikku.m84@gmail.com>
2016-06-06 11:36:42 +0530

    fixed payout

--- lib/payoneer/payout.rb
+++ lib/payoneer/payout.rb
@@ -2,7 +2,7 @@ module Payoneer
   class Payout
     CREATE_PAYOUT_API_METHOD_NAME = 'PerformPayoutPayment'
 
-    def self.create(program_id:, payment_id:, payee_id:, amount:, description:, payment_date: Time.now, currency: 'USD')
+    def self.create(program_id, payment_id, payee_id, amount, description, payment_date: Time.now, currency: 'USD')
       payoneer_params = {
         p4: program_id,
         p5: payment_id,
